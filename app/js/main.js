6c493505dc765ceb2f6a9b7db7efbeaba9864deb
Kirill Kuleshov <nstkuleshov1@gmail.com>
2019-07-02 22:35:16 +0300

    start stack

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,8 @@
 
+//=include ../components/header-about/header-about.js
+//=include ../components/borrow/borrow.js
+//=include ../components/help/help.js
+//=include ../components/slogan/slogan.js
+//=include ../components/story/story.js
+//=include ../components/contact/contact.js
 //=include ../components/footer/footer.js

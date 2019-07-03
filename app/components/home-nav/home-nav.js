a22846d088ebd2da942fe31c1b015ec5804438a8
Kirill Kuleshov <nstkuleshov1@gmail.com>
2019-07-04 00:53:49 +0300

    home-nav

--- app/components/home-nav/home-nav.js
+++ app/components/home-nav/home-nav.js
@@ -1,3 +1,16 @@
+
 $('.home-nav__ham').click(function (){
     $('.mob-menu').addClass('active');
+});
+
+$('.mob-menu__close').click(function (){
+    $('.mob-menu').removeClass('active');
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+        $('.home-nav').addClass('active');
+    } else {
+        $('.home-nav').removeClass('active');
+    }
 });
